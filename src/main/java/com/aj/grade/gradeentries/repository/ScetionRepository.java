983fca47147b50ba010247cb12ad6ac652bd999b
poran200 <44917375+poran200@users.noreply.github.com>
2019-09-29 15:09:53 +0600

    Update ScetionRepository.java

--- src/main/java/com/aj/grade/gradeentries/repository/ScetionRepository.java
+++ src/main/java/com/aj/grade/gradeentries/repository/ScetionRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 @Transactional
 public interface ScetionRepository extends JpaRepository<Section, String> {
-    List<Section> findAllByCourseCode(String courseCode);
+    List<Section> findAllByCourseCodeContaining(String courseCode);
 
     List<Section> findAllByFaculty(String faculty);
 
